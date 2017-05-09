7d6a37615491f916129f77f53af23d98b68e3d38
Gokhansayilgan <gokhansayilgan@gmail.com>
2017-05-09 18:32:47 +0300

    Sign in ui custom classes completed

---
+++ socialApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  socialApp
+//
+//  Created by Gökhan Sayılgan on 09/05/17.
+//  Copyright © 2017 Gökhan Sayılgan. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0 
---
+++ socialApp/FancyBtn.swift
@@ -0,0 +1,23 @@
+//
+//  FancyBtn.swift
+//  socialApp
+//
+//  Created by Gökhan Sayılgan on 09/05/17.
+//  Copyright © 2017 Gökhan Sayılgan. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6 ).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width:1.0 , height: 1.0 )
+        layer.cornerRadius = 10.0
+    }
+
+}
